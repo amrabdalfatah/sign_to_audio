7314996bc3009bce1f76e23386c13adfb4340387
amr <amr@amrs-MacBook-Air.local>
2025-05-13 14:11:25 +0300

    start camera permission

--- new.py
+++ new.py
@@ -183,17 +183,17 @@ def main():
         stop_button = st.button("إيقاف")
         ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
 
-        fig_place = st.empty()
-        fig, ax = plt.subplots(1, 1)
+        # fig_place = st.empty()
+        # fig, ax = plt.subplots(1, 1)
 
         while ctx.state.playing:
             with lock:
                 img = img_container["img"]
             if img is None:
                 continue
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            ax.cla()
-            ax.hist(gray.ravel(), 256, [0, 256])
+            # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # ax.cla()
+            # ax.hist(gray.ravel(), 256, [0, 256])
             # fig_place.pyplot(fig)
 
         # cap = cv2.VideoCapture(0)
